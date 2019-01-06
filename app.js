7f72814670f5f648bc0a2394561f628ab253164c
Furqan17 <36116798+Furqan17@users.noreply.github.com>
2019-01-06 13:39:59 -0600

    Install all dependencies

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const path = require('path');
+const methodOverride = require('method-override');
 const exphbs = require('express-handlebars');
+const flash = require('connect-flash');
 const bodyParser = require('body-parser');
+const passport = require('passport');
 const mongoose = require('mongoose');
+const session = require('express-session');
 
 // Init express
 const app = express();
@@ -39,9 +43,36 @@ app.use(bodyParser.json());
 // Static Folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Method Override Middleware
+app.use(methodOverride('_method'));
+
+// Express-Session Middleware
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+
+// NOTE: Express-Session middleware has to precede Passport middleware
+
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect-Flash Middleware
+app.use(flash());
+
+// Global Message Middleware
+app.use(function (req, res, next) {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    res.locals.user = req.user || null;
+    next();
+});
 
 // ----------
-// Node Routes
+// Express Routes
 
 // Index route (landing page)
 app.get('/', (req, res) => {
