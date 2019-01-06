80a640abd4f93b3d0e48d9d3405ed26dba08c70f
Furqan17 <36116798+Furqan17@users.noreply.github.com>
2019-01-06 12:35:01 -0600

    DB Config

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+// Conditionals for production vs development database keys
+if (process.env.NODE_ENV === 'production'){
+    module.exports = require('./keys_prod');
+} else {
+    module.exports = require('./keys_dev');
+}
---
+++ config/keys_prod.js
@@ -0,0 +1,4 @@
+// Production keys using NODE_ENV
+module.exports = {
+    mongoURI:process.env.MONGO_URI,
+}
