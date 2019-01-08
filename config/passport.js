c7f3f3241f331d47aa060f3d4ff9e50ec7a31b8b
Furqan17 <36116798+Furqan17@users.noreply.github.com>
2019-01-07 22:58:53 -0600

    Local Passport Strategy

--- config/passport.js
+++ config/passport.js
@@ -4,3 +4,35 @@ const bcrypt = require('bcryptjs');
 
 // Load user model
 const User = mongoose.model('users');
+
+module.exports = function(passport) {
+    passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+
+        // User existence
+        User.findOne({ email: email }).then(user => {
+            if (!user) {
+                // Return error msg
+                return done(null, false, { message: 'User not found' });
+            }
+
+            // Match password using bcrypt hashing
+            bcrypt.compare(password, user.password, (err, isMatch) => {
+                if (err) throw err;
+                if (isMatch) {
+                    return done(null, user);
+                } else {
+                    return done(null, false, { message: 'Incorrect Password' });
+                }
+            });
+        })
+    }));
+    passport.serializeUser(function(user, done) {
+        done(null, user.id);
+    });
+
+    passport.deserializeUser(function(id, done) {
+        User.findById(id, function(err, user) {
+            done(err, user);
+        })
+    })
+}
